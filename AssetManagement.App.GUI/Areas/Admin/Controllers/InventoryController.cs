6c2549f7f1dfeec69beca5ed02035afdff4b5384
alpsencerozen <alpsencerozen@gmail.com>
2022-04-01 03:26:55 +0300

    Started implementing create asset page with modal approach. Ajax get method

---
+++ AssetManagement.App.GUI/Areas/Admin/Controllers/InventoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagement.App.GUI.Areas.Admin.Controllers
+{
+    public class InventoryController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddAsset()
+        {
+            return RedirectToAction("Index", "Inventory");
+        }
+    }
+}
